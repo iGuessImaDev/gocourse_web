74c786a31ac4012804a4f2b61e3fd94fcdc117a4
Sebastián Gallardo <iguesimadev@gmail.com>
2026-08-02 16:07:43 -0400

    first commit

---
+++ internal/user/endpoint.go
@@ -0,0 +1,101 @@
+package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type (
+	Controller func(w http.ResponseWriter, r *http.Request)
+
+	Endpoints struct {
+		Create Controller
+		Get    Controller
+		GetAll Controller
+		Update Controller
+		Delete Controller
+	}
+
+	CreateReq struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Email     string `json:"email"`
+		Phone     string `json:"phone"`
+	}
+
+	ErrorRes struct {
+		Error string `json:"error"`
+	}
+)
+
+func MakeEndpoints(s Service) Endpoints {
+	return Endpoints{
+		Create: makeCreateEndpoint(s),
+		Get:    makeGetEndpoint(s),
+		GetAll: makeGetAllEndpoint(s),
+		Update: makeUpdateEndpoint(s),
+		Delete: makeDeleteEndpoint(s),
+	}
+}
+
+func makeCreateEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var req CreateReq
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(ErrorRes{"invalid request format"})
+			return
+		}
+
+		if req.FirstName == "" {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(ErrorRes{"first name is required"})
+			return
+		}
+
+		if req.LastName == "" {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(ErrorRes{"last name is required"})
+			return
+		}
+
+		err := s.Create(req.FirstName, req.LastName, req.Email, req.Phone)
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(ErrorRes{err.Error()})
+			return
+		}
+
+		json.NewEncoder(w).Encode(req)
+	}
+}
+
+func makeGetEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Get user")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeGetAllEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Get all user")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeUpdateEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Update user")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeDeleteEndpoint(s Service) Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Delete user")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/iGuessImaDev/gocourse_web/internal/user"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	userSrv := user.NewService()
+	userEnd := user.MakeEndpoints(userSrv)
+
+	router.HandleFunc("/users", userEnd.Create).Methods("POST")
+	router.HandleFunc("/users", userEnd.GetAll).Methods("GET")
+	router.HandleFunc("/users", userEnd.Update).Methods("PATCH")
+	router.HandleFunc("/users", userEnd.Delete).Methods("DELETE")
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
