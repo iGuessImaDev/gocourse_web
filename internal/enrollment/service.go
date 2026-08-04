cf940f7c793063769f6f4c30fb2ab4504b7c4d18
Sebastián Gallardo <iguesimadev@gmail.com>
2026-08-03 21:18:49 -0400

    fix validation on enrollment service

--- internal/enrollment/service.go
+++ internal/enrollment/service.go
@@ -1,9 +1,12 @@
 package enrollment
 
 import (
+	"errors"
 	"log"
 
+	"github.com/iGuessImaDev/gocourse_web/internal/course"
 	"github.com/iGuessImaDev/gocourse_web/internal/domain"
+	"github.com/iGuessImaDev/gocourse_web/internal/user"
 )
 
 type (
@@ -12,15 +15,19 @@ type (
 	}
 
 	service struct {
-		log  *log.Logger
-		repo Repository
+		log       *log.Logger
+		userSrv   user.Service
+		courseSrv course.Service
+		repo      Repository
 	}
 )
 
-func NewService(l *log.Logger, repo Repository) Service {
+func NewService(l *log.Logger, userSrv user.Service, courseSrv course.Service, repo Repository) Service {
 	return &service{
-		log:  l,
-		repo: repo,
+		log:       l,
+		userSrv:   userSrv,
+		courseSrv: courseSrv,
+		repo:      repo,
 	}
 }
 
@@ -29,6 +36,14 @@ func (s service) Create(userID, courseID string) (*domain.Enrollment, error) {
 		UserId:   userID,
 		CourseId: courseID,
 		Status:   "P",
+	}
+
+	if _, err := s.userSrv.Get(enroll.UserId); err != nil {
+		return nil, errors.New("user id doesn't exist!")
+	}
+
+	if _, err := s.courseSrv.Get(enroll.CourseId); err != nil {
+		return nil, errors.New("course id doesn't exist!")
 	}
 
 	if err := s.repo.Create(&enroll); err != nil {
